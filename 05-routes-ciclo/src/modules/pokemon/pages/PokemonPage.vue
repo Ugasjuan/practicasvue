1b6024e8b825acf10d5e2e9040eb551846e5ffcb
Eduard <guevaraeduard97@gmail.com>
2023-03-03 09:46:17 -0400

    router actualizado

--- 05-routes-ciclo/src/modules/pokemon/pages/PokemonPage.vue
+++ 05-routes-ciclo/src/modules/pokemon/pages/PokemonPage.vue
@@ -2,16 +2,51 @@ import { default } from '../../../App.vue';
 <template>
 
     <h1>Pokemon: <span># {{ id }}</span></h1>
+    <div v-if="pokemon">
+        <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+    </div>
 
 </template>
 
 <script>
+
     export default{
         
         props: {
             id:{
                 type: Number,
                 required: true
+            }
+        },
+        data(){
+            
+            return {
+                pokemon: null
+            }
+            
+        },
+        created() {
+            this.getPokemon()
+        },
+        methods: {
+            async getPokemon(){
+                
+                try {
+                    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${ this.id }`).then(r => r.json())
+                    console.log(pokemon);
+                    this.pokemon = pokemon
+                }    
+                catch (error) {
+                    this.$router.push('/')
+                    console.log('No hay nada que hacer aqui');   
+                }
+            }
+            
+        },
+        //A la escucha de algun cambio de la propiedad reactiva que le indiquemos
+        watch:{
+            id(){
+                this.getPokemon()
             }
         }
     }
