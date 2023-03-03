3a9f97b7c74599cac8d2ba7998946b8d627f33f2
Eduard <guevaraeduard97@gmail.com>
2023-03-03 18:31:34 -0400

    nuevo

---
+++ journal/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Hola Eduad</h1>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-success">Success</button>
+    <button class="btn btn-secondary">Secondary</button>
+  </div>
+</template>
+
